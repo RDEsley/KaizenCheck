c2a39e20a26e45b1d4a9057d2df8708936183bb9
Richard Oliveira <141041229+RDEsley@users.noreply.github.com>
2026-03-24 14:38:25 -0300

    Delete app directory

--- app/src/main/java/com/kaizencheck/app/data/ChecklistWithCount.kt
+++
@@ -1,9 +0,0 @@
-package com.kaizencheck.app.data
-
-import androidx.room.Embedded
-
-data class ChecklistWithCount(
-    @Embedded val checklist: ChecklistEntity,
-    val completedCount: Int,
-    val totalCount: Int
-)
